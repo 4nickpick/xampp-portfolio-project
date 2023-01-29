998abe2ab58910515f627f8584aa113ad3405ec8
Nick Pickering <4nickpick@gmail.com>
2023-01-29 05:16:31 -0500

    API controller working!

--- api/includes/bootstrap.php
+++ api/includes/bootstrap.php
@@ -17,21 +17,25 @@ else {
 // instantiate our dependencies
 require_once PROJECT_ROOT_PATH . "/vendor/autoload.php";
 
+
+// include the base controller
+require PROJECT_ROOT_PATH . "/controllers/BaseController.php";
+
 // include our repositories
 require_once PROJECT_ROOT_PATH . "/repositories/BaseRepository.php";
 require_once PROJECT_ROOT_PATH . "/repositories/UserRepository.php";
 
-$userRepository = new UserRepository(); 
+// $userRepository = new UserRepository(); 
 
 // $userRepository->create('Bobby', 'Tables', 'bobby@bobbytables.com', 'mypassword');
 
-$login = $userRepository->login('bobby@bobbytables.com', 'pikachu');
-var_dump($login);
+// $login = $userRepository->login('bobby@bobbytables.com', 'pikachu');
+// var_dump($login);
 
-$userRepository->resetPassword(7, 'pikachu');
+// $userRepository->resetPassword(7, 'pikachu');
 
-$login = $userRepository->login('bobby@bobbytables.com', 'pikachu');
-var_dump($login);
+// $login = $userRepository->login('bobby@bobbytables.com', 'pikachu');
+// var_dump($login);
 
 // if($userLogin) {
 //     echo "Welcome, {$userLogin['FirstName']}!";
