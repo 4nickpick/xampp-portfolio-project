a802ef72eff914902dccae3a3ef628dc488fd54f
Nick Pickering <4nickpick@gmail.com>
2023-02-02 16:52:32 -0500

    setting up routing

--- app/myprojectcom/src/app/app-routing.module.ts
+++ app/myprojectcom/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+]; // sets up routes constant where you define your routes
 
+// configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
